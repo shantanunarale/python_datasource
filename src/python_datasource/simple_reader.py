284c03cc01c0105d06830ded06bb1800a109ba38
Shantanu Narale <32578900+shantanunarale@users.noreply.github.com>
2026-04-19 18:18:44 +0530

    Rename classes to follow Python naming conventions

--- src/python_datasource/simple_reader.py
+++ src/python_datasource/simple_reader.py
@@ -2,20 +2,19 @@ from pyspark.sql.datasource import DataSource, DataSourceReader
 from pyspark.sql.types import StructType
 import faker as f    
 
-class simple_reader(DataSourceReader):
+class SimpleReader(DataSourceReader):
 
     def __init__(self, schema, options):
         self.schema = schema
         self.options = options
 
     def read(self, partition):
-        new_variable = 59
         fake = f.Faker('en_IN')
         number_of_rows = int(self.options.get('number_of_rows', 100))
         for i in range(number_of_rows):
             yield(i, fake.first_name(), fake.last_name())
 
-class simple_source(DataSource):
+class SimpleSource(DataSource):
     @classmethod
     def name(cls):
         return "simple"
